4f6b9ba48a77ac80dffb1800fdd1aa3be7c017c8
nicoolasmr <nicoolascf5@gmail.com>
2026-03-18 13:32:26 -0300

    refactor: remover alias global e padronizar imports dos testes

--- __tests__/components.test.tsx
+++ __tests__/components.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { Button } from '@/components/ui/button'
+import { Button } from '../components/ui/button'
 import { describe, it, expect } from 'vitest'
 import React from 'react'
 
